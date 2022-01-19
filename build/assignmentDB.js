29f1810b15551c7815f2d0a24840947b725c0072
Shawn Jacobsen <shawn.jacobsen0@gmail.com>
2022-01-19 12:57:34 -0500

    Delete Build Directory

--- build/assignmentDB.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addAssignment = void 0;
-function addAssignment(category = "", _class = "", assignmentName = "", progress = "Incomplete", dueDate = "", assignmentType = "Book Quiz", submission = "", quickNotes = "", canvasID = "", semester = "", DoToday = false) {
-    const updatedDatabase = 123;
-}
-exports.addAssignment = addAssignment;
