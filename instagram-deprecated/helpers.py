29745ef042c7ece473ae2e53541f4a53b12c34dc
shaj684 <48861103+shaj684@users.noreply.github.com>
2023-01-02 17:01:27 -0500

    deprecating instagram bot (really just not worth it lol)

--- instagram-deprecated/helpers.py
+++ instagram-deprecated/helpers.py
@@ -1,17 +1,18 @@
 # Instagram helper functions
-
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from helpers import sleepRand
+from instagram.pages import is404Page
 
 
-def loginToInstagram(dvr:WebDriver)-> (bool, str):
+def loginToInstagram(dvr:WebDriver, username:str, password:str)-> (bool, str):
   """ login to instagram
   Args:
       dvr (WebDriver): Selenium webdriver object
       implicit_wait (int): max time to wait on next element to load
   Returns:
       (bool, str): (did login?, "success" || <error message>)
-  """  
+  """
   res = (True, "success")
   loginURL = "https://www.instagram.com/accounts/login/"
   homeURL = "https://www.instagram.com/"
@@ -22,17 +23,21 @@ def loginToInstagram(dvr:WebDriver)-> (bool, str):
     if is404Page(dvr):
       res = (False, "redirected to 404 page")
     elif isLoginPage:
+      sleepRand(5,1,True)
       # login page elements
-      usernamme_input = dvr.find_element(By.NAME,"username")
+      username_input = dvr.find_element(By.NAME,"username")
       password_input = dvr.find_element(By.NAME,"password")
       login_btn = dvr.find_element(By.CSS_SELECTOR, "#loginForm > div > div:nth-child(3) > button")
 
       # fill and submit login form
-      usernamme_input.send_keys(USERNAME)
-      password_input.send_keys(PASSWORD)
+      username_input.send_keys(username)
+      password_input.send_keys(password)
+      sleepRand(2,1,True)
       login_btn.click()
       
       # approve one tap to stay logged in if prompted
+      sleepRand(5,3,True)
+      print(dvr.current_url)
       isOneTapPrompt = dvr.current_url == "https://www.instagram.com/accounts/onetap/"
       if isOneTapPrompt:
         save_info_btn = dvr.find_element(By.CSS_SELECTOR, "#mount_0_0_lo section > main > div > div > div > section > div > button")
--- instagram-deprecated/unfollow.py
+++ instagram-deprecated/unfollow.py
@@ -9,9 +9,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from helpers import scriptDirectory
+from helpers import scriptDirectory, sleepRand
 from instagram.helpers import loginToInstagram
-from instagram.pages import is404Page
 
 ### SETUP
 CHROME_DRIVER_LOC = f"{scriptDirectory()}/chromedriver.exe"
@@ -32,20 +31,24 @@ dvr = webdriver.Chrome(options=chrome_options)
 dvr.implicitly_wait(IMPLICIT_WAIT_TIMEOUT)
 
 # login
-didLogin, res_message = loginToInstagram(dvr)
+didLogin, res_message = loginToInstagram(dvr,USERNAME,PASSWORD)
 if not didLogin:
   print(f"ERROR: could not login to instagram. {res_message}")
-  print("Stopping process")
+  print("Stopping process...")
+  #dvr.quit()
   ### should send message to client via smtp server, too
   exit()
 
 # navigate to profile page, open followings modal
+homeURL = "https://www.instagram.com/"
 dvr.get(f"{homeURL}{USERNAME}/")
 following_selector = "#mount_0_0_vo section > main > div > header > section > ul > li:nth-child(3) > a"
 following_btn = dvr.find_element(By.CSS_SELECTOR, following_selector)
 following_btn.click()
+sleepRand(5,1)
+following_item_selector = "#mount_0_0_Az > div > div > div > div:nth-child(4) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._aano > div:nth-child(1) > div > div"
+all_following = dvr.find_elements(By.CSS_SELECTOR, following_item_selector)
+print(all_following)
 
 
-
-print("beginning endinig sleep")
 print("done.")
